0b557eb20ea9da314e0b0617251f3a53cf8730b5
Venky <venkatramanm@gmail.com>
2024-03-19 19:16:22 +0530

    KYC Default

--- src/main/java/in/succinct/beckn/registry/extensions/BeforeValidateNetworkParticipant.java
+++ src/main/java/in/succinct/beckn/registry/extensions/BeforeValidateNetworkParticipant.java
@@ -1,8 +1,11 @@
 package in.succinct.beckn.registry.extensions;
 
+import com.venky.swf.db.Database;
 import com.venky.swf.db.extensions.BeforeModelValidateExtension;
 import com.venky.swf.exceptions.AccessDeniedException;
+import in.succinct.beckn.registry.db.model.onboarding.DocumentPurpose;
 import in.succinct.beckn.registry.db.model.onboarding.NetworkParticipant;
+import in.succinct.beckn.registry.db.model.onboarding.SubmittedDocument;
 
 public class BeforeValidateNetworkParticipant extends BeforeModelValidateExtension<NetworkParticipant> {
     static {
@@ -14,6 +17,9 @@ public class BeforeValidateNetworkParticipant extends BeforeModelValidateExtensi
             if (!model.getReflector().getJdbcTypeHelper().getTypeRef(boolean.class).getTypeConverter().valueOf(model.getTxnProperty("kyc.complete"))){
                 throw new AccessDeniedException();
             }
+        }
+        if (Database.getTable(DocumentPurpose.class).recordCount() == 0){
+            model.setKycComplete(true);
         }
     }
 }
