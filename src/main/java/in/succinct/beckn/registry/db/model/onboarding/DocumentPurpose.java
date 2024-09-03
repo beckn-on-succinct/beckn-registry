3ae78d02b1bed2e614a0886442c80c640e73e4ad
Venky <venkatramanm@gmail.com>
2024-09-03 11:13:20 +0530

    Document purpose

--- src/main/java/in/succinct/beckn/registry/db/model/onboarding/DocumentPurpose.java
+++ src/main/java/in/succinct/beckn/registry/db/model/onboarding/DocumentPurpose.java
@@ -1,9 +1,11 @@
 package in.succinct.beckn.registry.db.model.onboarding;
 
 import com.venky.swf.db.annotations.column.UNIQUE_KEY;
+import com.venky.swf.db.annotations.model.MENU;
 import com.venky.swf.db.model.Model;
 
 
+@MENU("Admin")
 public interface DocumentPurpose extends Model {
     @UNIQUE_KEY
     public String getName();
